05e4bd331e97ba5e294772c46e7f90ee13600685
Diyari <dearyfareq15@gmail.com>
2023-03-21 22:55:53 +0300

    added html elements

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <input placeholder='City Name' type="text"/>
+      <div><h2>dallas</h2></div>
+      <div><h1>200f</h1><h2>weather</h2></div>
+      <div><h2>discription</h2></div>
+      <div><h2>time</h2></div>
     </div>
   );
 }
